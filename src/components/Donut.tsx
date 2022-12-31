59c561d3b5d58e3947a03228c62011cbea04d1a3
CadeWeiskopf <weiskopfcd@gmail.com>
2022-12-31 16:09:14 -0500

    struggle is real

--- src/components/Donut.tsx
+++ src/components/Donut.tsx
@@ -1,20 +1,51 @@
-import { useRef } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
+import { useEffect, useRef } from "react";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
 import * as THREE from "three";
+import { Typography } from "@mui/material";
 
 const Donut: React.FC = () => {
-  const mesh = useRef<MeshProps>(null);
+  return (
+    <div>
+      <Typography variant="h6">donut</Typography>
+      <Canvas style={{ height: "500px" }}>
+        <Scene />
+      </Canvas>
+    </div>
+  );
+};
+
+const Scene: React.FC = () => {
+  const mesh = useRef<THREE.Mesh>();
+  const { scene } = useThree();
   useFrame(() => {
-    if (mesh.current === null) {
+    if (mesh.current === undefined) {
       return;
     }
-    mesh.current.rotation.x = mesh.current.rotation.y += 0.01;
+
+    console.log(mesh.current);
+    mesh.current.rotation.x += 0.01;
+    mesh.current.rotation.y += 0.01;
   });
+
+  useEffect(() => {
+    const geometry = new THREE.TorusGeometry(1, 0.5, 16, 100);
+    const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+    mesh.current = new THREE.Mesh(geometry, material);
+
+    const light = new THREE.PointLight(0xffffff, 1, 1000);
+    light.position.set(50, 50, 50);
+    scene.add(light);
+  }, []);
+
   return (
-    <mesh {...props} ref={mesh} scale={[1.5, 1.5, 1.5]}>
-      <primitive object={allShapes[props.shape]} attach={"geometry"} />
-      <meshStandardMaterial color={allColors[props.shape]} />
-    </mesh>
+    <>
+      <pointLight position={[10, 10, 10]} />
+      <perspectiveCamera position={[0, 0, 5]} />
+      <mesh ref={mesh}>
+        <torusGeometry attach="geometry" args={[1, 0.5, 16, 100]} />
+        <meshBasicMaterial attach="material" color={0xff0000} />
+      </mesh>
+    </>
   );
 };
 
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,9 +1,12 @@
 import { Container, Box, Typography } from "@mui/material";
+import { useRef } from "react";
+import Donut from "./Donut";
 
 const Main: React.FC = () => {
   return (
     <Container maxWidth="lg">
       <Box my={2} width={"100%"} style={{ textAlign: "center" }}></Box>
+      <Donut />
     </Container>
   );
 };
