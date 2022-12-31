03192b11fdff20f96c0cc439c9f5ee0179fbde00
CadeWeiskopf <weiskopfcd@gmail.com>
2022-12-31 11:11:30 -0500

    Not doing the pop out for now.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,27 +5,7 @@ interface NavBarProps {
   className: string;
 }
 
-interface NavBarState {
-  anchorEl: HTMLElement | null;
-}
-
 const NavBar: React.FC<NavBarProps> = ({ className }) => {
-  const [state, setState] = useState<NavBarState>({
-    anchorEl: null,
-  });
-
-  const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
-    setState({
-      anchorEl: event.currentTarget,
-    });
-  };
-
-  const handleClose = () => {
-    setState({
-      anchorEl: null,
-    });
-  };
-
   return (
     <AppBar position="static" className={className}>
       <Toolbar>
@@ -33,23 +13,9 @@ const NavBar: React.FC<NavBarProps> = ({ className }) => {
           variant="h6"
           aria-controls="simple-menu"
           aria-haspopup="true"
-          onClick={handleClick}
         >
           DumbGames
         </Typography>
-        <Menu
-          id="simple-menu"
-          anchorEl={state.anchorEl}
-          anchorOrigin={{ vertical: "top", horizontal: "right" }}
-          transformOrigin={{ vertical: "top", horizontal: "left" }}
-          keepMounted
-          open={Boolean(state.anchorEl)}
-          onClose={handleClose}
-        >
-          <MenuItem onClick={handleClose}>Home</MenuItem>
-          <MenuItem onClick={handleClose}>About</MenuItem>
-          <MenuItem onClick={handleClose}>Contact</MenuItem>
-        </Menu>
       </Toolbar>
     </AppBar>
   );
