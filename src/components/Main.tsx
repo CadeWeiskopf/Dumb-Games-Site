269cf3e93b9e616523089565abb690d13e6166f7
CadeWeiskopf <weiskopfcd@gmail.com>
2022-12-31 12:17:01 -0500

    centering on main

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -3,7 +3,7 @@ import { Container, Box } from "@material-ui/core";
 const Main: React.FC = () => {
   return (
     <Container maxWidth="lg">
-      <Box my={2} width={"100%"}>
+      <Box my={2} width={"100%"} style={{ textAlign: "center" }}>
         <h2>Welcome to my app!</h2>
         <p>This is the main section of my app.</p>
       </Box>
