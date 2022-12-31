d99a62b2961d6f9074d2c4a4a3bf104ff4fb28ac
CadeWeiskopf <weiskopfcd@gmail.com>
2022-12-31 12:30:39 -0500

    dumb

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -4,8 +4,8 @@ const Main: React.FC = () => {
   return (
     <Container maxWidth="lg">
       <Box my={2} width={"100%"} style={{ textAlign: "center" }}>
-        <h2>Welcome to my app!</h2>
-        <p>This is the main section of my app.</p>
+        <h2>Welcome to Dumb Games!</h2>
+        <p>Have fun poking around.</p>
       </Box>
     </Container>
   );
