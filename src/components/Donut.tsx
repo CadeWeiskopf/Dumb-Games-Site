0c4e81d865efe8be3cbbe0d9533ba2dec0892a66
CadeWeiskopf <weiskopfcd@gmail.com>
2023-01-02 20:23:22 -0500

    Using math to generate the Sprinkles!!

--- src/components/Donut.tsx
+++ src/components/Donut.tsx
@@ -15,8 +15,56 @@ const Donut: React.FC = () => {
   );
 };
 
+const generateSprinkles = () => {
+  const sprinkles: {
+    position: THREE.Vector3;
+    rotation: THREE.Euler;
+    color: number;
+  }[] = [];
+
+  // Generate a higher number of sprinkles to ensure that a majority of the donut is covered
+  const numSprinkles = 200;
+
+  const colors = [0x06b583, 0xefdd00, 0xf99f0d, 0x9c2fbc, 0xe30427, 0x0994e3];
+
+  for (let i = 0; i < numSprinkles; i++) {
+    // Generate random angle and radius values
+    const angle = Math.random() * 2 * Math.PI;
+    const radius = 0.6 + Math.random() * 0.6; // radius of donut is between 0.6 and 1
+
+    // Calculate position based on angle and radius
+    const x = radius * Math.cos(angle);
+    const y = radius * Math.sin(angle);
+    const z = 0.5; // front of donut
+
+    const position = new THREE.Vector3(x, y, z);
+
+    // Generate random rotation
+    const rotation = new THREE.Euler(
+      Math.random() * 2 * Math.PI,
+      Math.random() * 2 * Math.PI,
+      Math.random() * 2 * Math.PI
+    );
+
+    // Generate random color
+    const color = colors[Math.floor(Math.random() * colors.length)];
+
+    sprinkles.push({ position, rotation, color });
+  }
+
+  return sprinkles;
+};
+
 const Scene: React.FC = () => {
   const [rotation, setRotation] = useState<number>(0);
+  const [sprinkles, setSprinkles] = useState<
+    { position: THREE.Vector3; rotation: THREE.Euler }[]
+  >([]);
+
+  // Generate the sprinkles when the component mounts
+  useEffect(() => {
+    setSprinkles(generateSprinkles());
+  }, []);
   const rafId = useRef<number>(0);
   const lastFrameTime = useRef<number>(0);
   const animate = (timeStamp: number) => {
@@ -55,159 +103,44 @@ const Scene: React.FC = () => {
     </mesh>
   );
 
-  // Create the sprinks
-  const sprinkleRef = useRef<Mesh>(null);
-  const sprinkle = (
-    <mesh ref={sprinkleRef} position={[0.05, 1, 0.5]} scale={0.025}>
-      <capsuleGeometry attach="geometry" args={[1, 3, 5, 16]} />
-      <meshBasicMaterial attach="material" color={0x9c2fbc} />
-    </mesh>
-  );
-  const sprinkle2Ref = useRef<Mesh>(null);
-  const sprinkle2 = (
-    <mesh
-      ref={sprinkle2Ref}
-      position={[0.2, 1, 0.5]}
-      scale={0.025}
-      rotation={[0, 0, 1]}
-    >
-      <capsuleGeometry attach="geometry" args={[1, 3, 5, 16]} />
-      <meshBasicMaterial attach="material" color={0x0994e3} />
-    </mesh>
-  );
-  const sprinkle3Ref = useRef<Mesh>(null);
-  const sprinkle3 = (
-    <mesh
-      ref={sprinkle3Ref}
-      position={[0.1, 0.85, 0.5]}
-      scale={0.025}
-      rotation={[0, 0, -1]}
-    >
-      <capsuleGeometry attach="geometry" args={[1, 3, 5, 16]} />
-      <meshBasicMaterial attach="material" color={0xf99f0d} />
-    </mesh>
-  );
-  const sprinkle4Ref = useRef<Mesh>(null);
-  const sprinkle4 = (
-    <mesh
-      ref={sprinkle4Ref}
-      position={[0.1, 1.15, 0.4]}
-      scale={0.025}
-      rotation={[0, 0, 0.8]}
-    >
-      <capsuleGeometry attach="geometry" args={[1, 3, 5, 16]} />
-      <meshBasicMaterial attach="material" color={0xefdd00} />
-    </mesh>
-  );
-  const sprinkle5Ref = useRef<Mesh>(null);
-  const sprinkle5 = (
-    <mesh
-      ref={sprinkle5Ref}
-      position={[0.1, 0.7, 0.42]}
-      scale={0.025}
-      rotation={[0, 0, -1.5]}
-    >
-      <capsuleGeometry attach="geometry" args={[1, 3, 5, 16]} />
-      <meshBasicMaterial attach="material" color={0x06b583} />
-    </mesh>
-  );
-
-  const sprinkle6Ref = useRef<Mesh>(null);
-  const sprinkle6 = (
-    <mesh ref={sprinkle6Ref} position={[0.05, 1, 0.5]} scale={0.025}>
-      <capsuleGeometry attach="geometry" args={[1, 3, 5, 16]} />
-      <meshBasicMaterial attach="material" color={0x9c2fbc} />
-    </mesh>
-  );
-  const sprinkle7Ref = useRef<Mesh>(null);
-  const sprinkle7 = (
-    <mesh
-      ref={sprinkle7Ref}
-      position={[0.2, 1, 0.5]}
-      scale={0.025}
-      rotation={[0, 0, 1]}
-    >
-      <capsuleGeometry attach="geometry" args={[1, 3, 5, 16]} />
-      <meshBasicMaterial attach="material" color={0x0994e3} />
-    </mesh>
-  );
-  const sprinkle8Ref = useRef<Mesh>(null);
-  const sprinkle8 = (
-    <mesh
-      ref={sprinkle8Ref}
-      position={[0.3, 0.7, 0.48]}
-      scale={0.025}
-      rotation={[-0.15, 0, -0.3]}
-    >
-      <capsuleGeometry attach="geometry" args={[1, 3, 5, 16]} />
-      <meshBasicMaterial attach="material" color={0xf99f0d} />
-    </mesh>
-  );
-  const sprinkle9Ref = useRef<Mesh>(null);
-  const sprinkle9 = (
-    <mesh
-      ref={sprinkle9Ref}
-      position={[0.4, 0.9, 0.5]}
-      scale={0.025}
-      rotation={[0, 0, 0.4]}
-    >
-      <capsuleGeometry attach="geometry" args={[1, 3, 5, 16]} />
-      <meshBasicMaterial attach="material" color={0xefdd00} />
-    </mesh>
-  );
-  const sprinkle10Ref = useRef<Mesh>(null);
-  const sprinkle10 = (
-    <mesh
-      ref={sprinkle10Ref}
-      position={[0.6, 0.7, 0.5]}
-      scale={0.025}
-      rotation={[0, 0, -1.2]}
-    >
-      <capsuleGeometry attach="geometry" args={[1, 3, 5, 16]} />
-      <meshBasicMaterial attach="material" color={0x06b583} />
-    </mesh>
-  );
-
-  if (
-    donutRef.current &&
-    sprinkleRef.current &&
-    sprinkle2Ref.current &&
-    sprinkle3Ref.current &&
-    sprinkle4Ref.current &&
-    sprinkle5Ref.current &&
-    sprinkle6Ref.current &&
-    sprinkle7Ref.current &&
-    sprinkle8Ref.current &&
-    sprinkle9Ref.current &&
-    sprinkle10Ref.current
-  ) {
-    donutRef.current.add(sprinkleRef.current);
-    donutRef.current.add(sprinkle2Ref.current);
-    donutRef.current.add(sprinkle3Ref.current);
-    donutRef.current.add(sprinkle4Ref.current);
-    donutRef.current.add(sprinkle5Ref.current);
-    donutRef.current.add(sprinkle6Ref.current);
-    donutRef.current.add(sprinkle7Ref.current);
-    donutRef.current.add(sprinkle8Ref.current);
-    donutRef.current.add(sprinkle9Ref.current);
-    donutRef.current.add(sprinkle10Ref.current);
-  }
-
   return (
     <>
       {donut}
-      {sprinkle}
-      {sprinkle2}
-      {sprinkle3}
-      {sprinkle4}
-      {sprinkle5}
-      {sprinkle6}
-      {sprinkle7}
-      {sprinkle8}
-      {sprinkle9}
-      {sprinkle10}
+      {sprinkles.map((sprinkle, index) => (
+        <Sprinkle
+          key={index}
+          position={sprinkle.position}
+          rotation={sprinkle.rotation}
+          donutRef={donutRef}
+          color={sprinkle.color}
+        />
+      ))}
     </>
   );
 };
 
+const Sprinkle: React.FC<{
+  position: THREE.Vector3;
+  rotation: THREE.Euler;
+  donutRef: React.RefObject<Mesh | undefined>;
+  color: number;
+}> = ({ position, rotation, donutRef, color }) => {
+  const sprinkleRef = useRef<Mesh>(null);
+  useEffect(() => {
+    if (donutRef.current && sprinkleRef.current) {
+      sprinkleRef.current.parent = donutRef.current;
+    }
+  }, []);
+  return (
+    <mesh
+      ref={sprinkleRef}
+      position={position}
+      rotation={rotation}
+      scale={0.025}
+    >
+      <capsuleGeometry attach="geometry" args={[1, 3, 5, 16]} />
+      <meshBasicMaterial attach="material" color={color} />
+    </mesh>
+  );
+};
 export default Donut;
